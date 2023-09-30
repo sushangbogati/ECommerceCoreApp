fad796f3815be7360dc47758ecd40182e217432d
sushang bogati <sushang_bogati@hotmail.com>
2023-09-30 14:35:09 +0545

    Account Controller Created

---
+++ ECommerceCoreApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerceCoreApp.Models;
+namespace ECommerceCoreApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignIn(SiteUser user)
+        {
+            return View();
+        }
+    }
+}
